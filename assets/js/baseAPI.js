7127c052f21d6f5fa2cf04f1cd62fd4fa0d52213
Yaxiang <liuyaxiang123@icloud.com>
2020-11-01 20:44:34 +0800

    完成首页内容区域数据渲染用户头像渲染退出功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,5 +4,19 @@ $(function () {
   $.ajaxPrefilter(function (options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+    // 给所有需要token权限的请求添加token
+    if (options.url.indexOf('/my/') !== -1) {
+      options.headers = {
+        Authorization: localStorage.getItem('token') || ''
+      }
+    }
+    // 给页面统一添加权限请求
+    options.complete = function (res) {
+      // 进行判断,complete里面会有那个返回值,status和message
+      if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        localStorage.removeItem('token')
+        location.href = '/login.html'
+      }
+    }
   })
 })
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+$(function () {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    // headers 就是请求头配置对象,将token放入本地存储中
+    // headers: {
+    //   Authorization: localStorage.getItem('token') || ''
+    // },
+    success: function (res) {
+      if (res.status !== 0) {
+        return layui.layer.msg('获取用户信息失败！')
+      }
+      // 调用 renderAvatar 渲染用户的头像
+      renderAvatar(res.data)
+    },
+    // 不管是成功还是失败都会调用这个函数;
+    // complete: function (res) {
+    //   // 进行判断,complete里面会有那个返回值,status和message
+    //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+    //     localStorage.removeItem('token')
+    //     location.href = '/login.html'
+    //   }
+    // }
+  })
+
+  // 渲染用户头像
+  function renderAvatar(user) {
+    let name = user.nickname || user.username
+    $('#welcome').html(`欢迎  ${name}`)
+    if (user.user_pic !== null) {
+      $('.layui-nav-img').attr('src', user.user_pic).show()
+      $('.text-avatar').hide()
+    } else {
+      $('.layui-nav-img').hide()
+      let first = name[0].toUpperCase()
+      $('.text-avatar').html(first).show()
+    }
+  }
+
+  //退出功能
+  $('#btnLogout').on('click', function () {
+    layer.confirm('确定退出?', { icon: 3, title: '提示 ' }, function (index) {
+      localStorage.removeItem('token')
+      location.href = '/login.html';
+      // layer 自带的代码; 关闭confirm询问框;关闭当前窗口
+      layer.close(index)
+    })
+  })
+})
