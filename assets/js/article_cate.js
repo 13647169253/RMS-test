013f6ffac70ceba623f5ef208c9ed3c3eedb5de6
Yaxiang <liuyaxiang123@icloud.com>
2020-11-02 16:05:31 +0800

    完成文章类别模块

--- assets/js/article_cate.js
+++ assets/js/article_cate.js
@@ -1,8 +1,11 @@
 $(function () {
-  let layer = layui.layer
-  let indexAdd = null
-
   initArtCateList()
+
+  let layer = layui.layer
+  let form = layui.form
+
+  let indexAdd = null
+  let indexEdit = null
 
   // 为添加类别按钮绑定点击事件
   $('#btnAddCate').on('click', function () {
@@ -13,7 +16,8 @@ $(function () {
       content: $('#dialog-add').html()
     })
   })
-  // 通过代理为form-add添加submit事件
+
+  // 添加 > 通过代理为form-add添加submit事件
   $('body').on('submit', '#form-add', function (e) {
     e.preventDefault()
     $.ajax({
@@ -32,6 +36,61 @@ $(function () {
     })
   })
 
+  // 编辑分类
+  $('tbody').on('click', '.btn-edit', function () {
+    // 弹出一个修改文章分类信息的层
+    indexEdit = layer.open({
+      type: 1,
+      area: ['500px', '250px'],
+      title: '修改文章分类',
+      content: $('#dialog-edit').html()
+    })
+    let id = $(this).attr('data-Id')
+    // 获取数据并使用form填充
+    $.ajax({
+      method: 'GET',
+      url: '/my/article/cates/' + id,
+      success: function (res) {
+        form.val('form-edit', res.data)
+      }
+    })
+    // 通过代理的形式，为修改分类的表单绑定 submit 事件
+    $('body').on('submit', '#form-edit', function (e) {
+      e.preventDefault()
+      $.ajax({
+        method: 'POST',
+        url: '/my/article/updatecate',
+        data: $(this).serialize(),
+        success: function (res) {
+          if (res.status !== 0) {
+            return layer.msg('更新分类数据失败！')
+          }
+          layer.msg('更新分类数据成功！')
+          layer.close(indexEdit)
+          initArtCateList()
+        }
+      })
+    })
+  })
+  //删除功能
+  $('tbody').on('click', '.btn-delete', function () {
+    let id = $(this).attr('data-id')
+    // 提示用户是否要删除
+    layer.confirm('确认删除?', { icon: 3, title: '提示' }, function (index) {
+      $.ajax({
+        method: 'GET',
+        url: '/my/article/deletecate/' + id,
+        success: function (res) {
+          if (res.status !== 0) {
+            return layer.msg('删除分类失败！')
+          }
+          layer.msg('删除分类成功！')
+          layer.close(index)
+          initArtCateList()
+        }
+      })
+    })
+  })
 })
 
 
