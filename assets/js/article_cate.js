97bfc32aecc089cdfc0330053dbe1857b79fb403
Yaxiang <liuyaxiang123@icloud.com>
2020-11-02 14:33:28 +0800

    完成文章类别添加

--- assets/js/article_cate.js
+++ assets/js/article_cate.js
@@ -1,16 +1,37 @@
 $(function () {
   let layer = layui.layer
+  let indexAdd = null
+
   initArtCateList()
 
   // 为添加类别按钮绑定点击事件
   $('#btnAddCate').on('click', function () {
-    layer.open({
+    indexAdd = layer.open({
       type: 1,
       area: ['500px', '250px'],
       title: '添加文章分类',
       content: $('#dialog-add').html()
     })
   })
+  // 通过代理为form-add添加submit事件
+  $('body').on('submit', '#form-add', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/my/article/addcates',
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('新增分类失败！')
+        }
+        initArtCateList()
+        layer.msg('新增分类成功！')
+        // 根据索引，关闭对应的弹出层
+        layer.close(indexAdd)
+      }
+    })
+  })
+
 })
 
 
