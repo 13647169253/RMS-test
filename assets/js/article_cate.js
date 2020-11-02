1a68217b7b8511488fc478bdb73ed54040089a33
Yaxiang <liuyaxiang123@icloud.com>
2020-11-02 14:18:26 +0800

    完成部分

---
+++ assets/js/article_cate.js
@@ -0,0 +1,27 @@
+$(function () {
+  let layer = layui.layer
+  initArtCateList()
+
+  // 为添加类别按钮绑定点击事件
+  $('#btnAddCate').on('click', function () {
+    layer.open({
+      type: 1,
+      area: ['500px', '250px'],
+      title: '添加文章分类',
+      content: $('#dialog-add').html()
+    })
+  })
+})
+
+
+// 获取文章分类的列表
+function initArtCateList() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/article/cates',
+    success: function (res) {
+      var htmlStr = template('tpl-table', res)
+      $('tbody').html(htmlStr)
+    }
+  })
+}
